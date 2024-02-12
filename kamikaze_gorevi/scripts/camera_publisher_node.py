65c39cea5ca7ab0125a3c11eb800c49c74052cf5
Yiğit <153460142+Valvar-N@users.noreply.github.com>
2024-02-12 11:50:37 +0300

    Update camera_publisher_node.py

--- kamikaze_gorevi/scripts/camera_publisher_node.py
+++ kamikaze_gorevi/scripts/camera_publisher_node.py
@@ -6,6 +6,7 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 from pyzbar import pyzbar
 import webbrowser
+import datetime
 
 def camera_publisher():
     rospy.init_node('camera_publisher', anonymous=True)
@@ -25,7 +26,9 @@ def camera_publisher():
     bridge = CvBridge()
 
     rate = rospy.Rate(30)  # Adjust the rate if needed
-
+    
+    qr_scanned = False # Control condition for scanning QR just once
+    
     while not rospy.is_shutdown():
         ret, frame = cap.read()
 
@@ -37,12 +40,18 @@ def camera_publisher():
             # Find and decode QR codes in the frame
             barcodes = pyzbar.decode(frame)
             for barcode in barcodes:
+                
                 # Extract QR code data
                 qr_data = barcode.data.decode("utf-8")
-                
+   
                 # Process QR code data (e.g., open URL)
-                if qr_data.startswith("http"):
+                if qr_data.startswith("http") and not qr_scanned:
                     webbrowser.open(qr_data)
+                    qr_scanned = True
+                elif qr_scanned:
+                    # Get the current system time
+                    current_time = datetime.datetime.now()
+                    print(str(current_time) + " QR Scanned")
                 else:
                     print("QR Code Text:", qr_data)
 
