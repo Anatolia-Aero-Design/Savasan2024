f148613342f4a53c2a0a5ae1f9f42f4640180db5
peteksnr <117526538+peteksnr@users.noreply.github.com>
2024-02-25 15:01:33 +0300

    Update follow_w_param.py

--- dive_in_out_and_gps/follow_w_param.py
+++ dive_in_out_and_gps/follow_w_param.py
@@ -4,16 +4,11 @@ import rospy
 from mavros_msgs.srv import CommandInt
 from sensor_msgs.msg import NavSatFix
 from mavros_msgs.msg import PlaneControl
-
-# FOLLOW PLANE FROM SERVER WITH PARAMETERS 
-
-# variables 
-team_number = 0
+ 
 follow = False
+leader_position = PlaneControl() 
 
-leader_position = PlaneControl() # our message file 
-
-def send_goto_position(target_latitude, target_longitude, target_altitude): # for sending command to the plane
+def send_goto_position(target_latitude, target_longitude, target_altitude):
     rospy.wait_for_service('/plane1/mavros/cmd/command_int')
     try:
         command_int = rospy.ServiceProxy('/plane1/mavros/cmd/command_int', CommandInt)
@@ -36,32 +31,30 @@ def send_goto_position(target_latitude, target_longitude, target_altitude): # fo
         rospy.logerr("Service call failed: %s"%e)
 
 def callback(data):
-    global leader_position, team_number
-    # update team_number when received new data
-    team_number = rospy.get_param('~team_number', leader_position.takim_numarasi) # team number is the parameter we entered
+    global leader_position
     leader_position = data
-    rospy.loginfo("Target Position: Lat %f, Lon %f, Alt %f", data.IHA_enlem, data.IHA_boylam, data.IHA_irtifa)
-    # check if follow is enabled and the team number matches
-    if rospy.get_param('~team_number') == leader_position.takim_numarasi:
-        if follow and leader_position.takim_numarasi == team_number: 
-            target_latitude = leader_position.IHA_enlem
-            target_longitude = leader_position.IHA_boylam
-            target_altitude = leader_position.IHA_irtifa   
-            send_goto_position(target_latitude, target_longitude, target_altitude) # send target teams coordinatess
-        else:
-            rospy.loginfo("Follow is disabled or team number doesn't match.")
-
+    if follow and rospy.get_param('~target_id') == leader_position.takim_numarasi:
+        rospy.loginfo("Target Position: Lat %f, Lon %f, Alt %f", data.IHA_enlem, data.IHA_boylam, data.IHA_irtifa)
+        target_latitude = leader_position.IHA_enlem
+        target_longitude = leader_position.IHA_boylam
+        target_altitude = leader_position.IHA_irtifa   
+        send_goto_position(target_latitude, target_longitude, target_altitude) 
+    else:
+        rospy.loginfo("target_id doesn't match.")
 
 def main():
-    global team_number, follow
-    rospy.init_node('send_goto_position_node')
-    # initial values
-    team_number = rospy.get_param('~team_number', 0)
+    global follow
+    rospy.init_node('gps_node')
     follow = rospy.get_param('~follow', False)
-    rospy.loginfo("Team Number: %d", team_number)
     rospy.loginfo("Follow: %s", follow)
     rospy.Subscriber('plane_coordinates_topic', PlaneControl, callback)
+    rospy.Timer(rospy.Duration(1),callback)
     rospy.spin()
+
+def callback(event):
+    global follow
+    follow = rospy.get_param('~follow', False)
+    rospy.loginfo("follow is: %s", follow)
 
 if __name__ == '__main__':
     main()
